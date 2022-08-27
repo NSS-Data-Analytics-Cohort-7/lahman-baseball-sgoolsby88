eae1426c38797ee288c170c30346d004449eb825
Steven <sgoolsby88@yahoo.com>
2022-08-27 11:13:31 -0500

    Finished_9

--- scripts.sql
+++ scripts.sql
@@ -127,7 +127,6 @@ Group by teamid
 Order By perc_win;
 
 --Q8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
-
 Select distinct h.team,
     p.park_name,
     sum(attendance)/sum(games) as avg_attendance
@@ -155,6 +154,27 @@ Limit 5;
 --A. Run Query for "Bottom 5 Avg_Attendance"
 
 --Q9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
-
+Select distinct p.namefirst, p.namelast, t.name as team
+From People as p
+Left Join awardsmanagers as am
+Using(playerid)
+Join managershalf as mh
+Using(playerid)
+Join teams as t
+Using(teamid)
+Where am.awardid = 'TSN Manager of the Year'
+    And am.lgid = 'NL'
+UNION
+Select distinct p.namefirst, p.namelast, t.name AS team 
+From People as p
+Left Join awardsmanagers as am
+Using(playerid)
+Join managershalf as mh
+Using(playerid)
+Join teams as t
+Using(teamid)
+Where am.awardid = 'TSN Manager of the Year'
+    And am.lgid = 'AL';
+--A. Run Query for list of TSN Manager of the Year winners.
 
 --Q10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
